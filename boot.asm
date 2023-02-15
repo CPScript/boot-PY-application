a649414f1f3c6395adac32ac38a5a10e8bd5db51
Chai <83523587+CPScript@users.noreply.github.com>
2023-02-15 14:12:06 -0500

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -48,6 +48,6 @@ cls:
 	int 0x10       ; Call the BIOS interrupt!
 	ret            ; Return
 
-display db "THis is...", 13, 10, "WANNA DIE", 13, 10, 0 ; The null terminator is very important!
+display db "This is...", 13, 10, "WANNA DIE", 13, 10, 0 ; The null terminator is very important!
 times 510 - ($ - $$) db 0 ; Zero remaining sectors
 dw 0xaa55 ; Bootable signature
